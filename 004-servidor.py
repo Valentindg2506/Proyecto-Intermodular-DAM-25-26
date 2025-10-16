ea6f255ad4a3b222d0ba59da18a2e39899141f26
Valentin Antonio de Gennaro <valentindegennaro@gmail.com>
2025-10-16 12:50:44 +0200

    main

--- 004-servidor.py
+++ 004-servidor.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import json
 
 aplicacion = Flask(__name__)
 
 @aplicacion.route("/")
 def raiz():
-	return '''
+########### ESTE ES UN BLOQUE ##################
+	cadena = '''
 	<!Doctype html>
 <html lang="es">
 	<head>
@@ -20,17 +22,28 @@ def raiz():
 	<body>
 		<header><h1>Valentinblog</h1></header>
 		<main>
-			<article>
-				<h3>Titulo del articulo</h3>
-				<time>2025-10-16</time>
-				<p>Valentin Antonio De Gennaro</p>
-				<p>Este es el contenido de un articulo ficticio</p>
-			</article>
+		'''
+########### ESTE ES OTRO BLOQUE #################
+	archivo = open("blog.json","r")
+	contenido = json.load(archivo)
+	for linea in contenido:
+		cadena += '''
+				<article>
+					<h3>'''+linea['titulo']+'''</h3>
+					<time>'''+linea['fecha']+'''</time>
+					<p>'''+linea['autor']+'''</p>
+					<p>'''+linea['contenido']+'''</p>
+				</article>
+				'''
+########### ESTE ES OTRO BLOQUE #################
+	cadena += '''
 		</main>
 		<footer>(c)2025 Valentin Antonio De Gennaro</footer>
 	</body>
 </html>
 '''
+########## NO OLVIDARSE DEL RETURN #############
+	return cadena
 	
 if __name__ == "__main__":
 	aplicacion.run(debug=True)
