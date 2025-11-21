7308c81088b0d2d612ff9fb971bee43ca09baf4e
Valentin Antonio de Gennaro <valentindegennaro@gmail.com>
2025-11-21 14:05:50 +0100

    21/11

--- Simulacro/004-Crear usuario.sql
+++ Simulacro/004-Crear usuario.sql
@@ -1,16 +1,16 @@
 -- crea usuario nuevo con contraseña
 -- creamos el nombre de usuario que queramos
 CREATE USER 
-'admin'@'localhost' 
+'admin1'@'localhost' 
 IDENTIFIED  BY 'Portafolio2526@';
 
 -- permite acceso a ese usuario
-GRANT USAGE ON *.* TO 'admin'@'localhost';
+GRANT USAGE ON *.* TO 'admin1'@'localhost';
 --[tuservidor] == localhost
 -- La contraseña puede requerir Mayus, minus, numeros, caracteres, min len
 
 -- quitale todos los limites que tenga
-ALTER USER 'admin'@'localhost'
+ALTER USER 'admin1'@'localhost'
 REQUIRE NONE 
 WITH MAX_QUERIES_PER_HOUR 0 
 MAX_CONNECTIONS_PER_HOUR 0 
@@ -19,7 +19,7 @@ MAX_USER_CONNECTIONS 0;
 
 -- dale acceso a la base de datos empresadam
 GRANT ALL PRIVILEGES ON `portafolio`.* 
-TO 'admin'@'localhost';
+TO 'admin1'@'localhost';
 
 -- recarga la tabla de privilegios
 FLUSH PRIVILEGES;
